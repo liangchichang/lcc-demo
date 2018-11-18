07448d1484c2f2939e3cb1d17ab04918981e06df
lcc <chichang.liang@greatonce.com>
2018-11-18 12:30:51 +0800

    添加多线程测试

---
+++ test/src/test/java/com/lcc/demo/volatiletest/JVMParmTest.java
@@ -0,0 +1,12 @@
+package com.lcc.demo.volatiletest;
+
+/**
+ * @author Lcc
+ * @version 2018/11/10
+ */
+public class JVMParmTest {
+
+  public static void main(String[] args) {
+    System.getProperties().list(System.out);
+  }
+}
