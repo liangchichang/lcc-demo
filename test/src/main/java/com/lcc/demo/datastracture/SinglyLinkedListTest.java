cfc3e8297fad28f49dce9cf083caa7842d146bdb
lcc <chichang.liang@greatonce.com>
2018-11-22 19:45:08 +0800

    增加单链表

--- test/src/main/java/com/lcc/demo/datastracture/SinglyLinkedListTest.java
+++ test/src/main/java/com/lcc/demo/datastracture/SinglyLinkedListTest.java
@@ -1,136 +1,63 @@
 package com.lcc.demo.datastracture;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
 /**
  * @author lcc
  * @version 2018/11/19
  *
  * 单向链表
  */
-public class SinglyLinkedListTest<T> implements List {
+public class SinglyLinkedListTest<T> {
 
   private transient static int nodeCount = 0;
   private transient Node<T> first;
   private transient Node<T> last;
 
-  @Override
+
+  public SinglyLinkedListTest() {
+    this.first = null;
+    this.last = null;
+  }
+
+  public SinglyLinkedListTest(T t) {
+    Node<T> node = new Node<>(t, null);
+    this.first = node;
+    this.last = node;
+  }
+
   public int size() {
     return nodeCount;
   }
 
-  @Override
   public boolean isEmpty() {
     return first == null;
   }
 
-  @Override
-  public boolean contains(Object o) {
-    return false;
+  public boolean add(T t) {
+    Node<T> node = new Node<>(t, null);
+    if(first == null){
+      this.first = node;
+      this.last = node;
+    }else{
+      this.last.next = node;
+      this.last = node;
+    }
+    return true;
   }
 
-  @Override
-  public Iterator iterator() {
-    return null;
-  }
+//  public boolean add(int index) {
+//
+//  }
+//
+//  public boolean delete(T t) {
+//
+//  }
+//
+//  public boolean delete(int index) {
+//
+//  }
 
-  @Override
-  public Object[] toArray() {
-    return new Object[0];
-  }
-
-  @Override
-  public boolean add(Object o) {
-
-    return false;
-  }
-
-  @Override
-  public boolean remove(Object o) {
-    return false;
-  }
-
-  @Override
-  public boolean addAll(Collection c) {
-    return false;
-  }
-
-  @Override
-  public boolean addAll(int index, Collection c) {
-    return false;
-  }
-
-  @Override
-  public void clear() {
-
-  }
-
-  @Override
-  public Object get(int index) {
-    return null;
-  }
-
-  @Override
-  public Object set(int index, Object element) {
-    return null;
-  }
-
-  @Override
-  public void add(int index, Object element) {
-
-  }
-
-  @Override
-  public Object remove(int index) {
-    return null;
-  }
-
-  @Override
-  public int indexOf(Object o) {
-    return 0;
-  }
-
-  @Override
-  public int lastIndexOf(Object o) {
-    return 0;
-  }
-
-  @Override
-  public ListIterator listIterator() {
-    return null;
-  }
-
-  @Override
-  public ListIterator listIterator(int index) {
-    return null;
-  }
-
-  @Override
-  public List subList(int fromIndex, int toIndex) {
-    return null;
-  }
-
-  @Override
-  public boolean retainAll(Collection c) {
-    return false;
-  }
-
-  @Override
-  public boolean removeAll(Collection c) {
-    return false;
-  }
-
-  @Override
-  public boolean containsAll(Collection c) {
-    return false;
-  }
-
-  @Override
-  public Object[] toArray(Object[] a) {
-    return new Object[0];
+  public T getLast() {
+    return last == null ? null : last.item;
   }
 
   private static class Node<T> {
