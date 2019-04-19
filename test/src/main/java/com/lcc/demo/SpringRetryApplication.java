347c2d252f063aa2e8f08c887833a37b41ec5b85
lcc <chichang.liang@greatonce.com>
2019-04-19 14:36:14 +0800

    code fix

--- test/src/main/java/com/lcc/demo/SpringRetryApplication.java
+++ test/src/main/java/com/lcc/demo/SpringRetryApplication.java
@@ -1,7 +1,5 @@
 package com.lcc.demo;
 
-import com.lcc.demo.retry.exception.StatefulRetryException;
-import com.lcc.demo.retry.exception.StatelessRetryException;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
@@ -12,12 +10,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.classify.BinaryExceptionClassifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.retry.RetryCallback;
 import org.springframework.retry.RetryContext;
 import org.springframework.retry.RetryListener;
 import org.springframework.retry.RetryState;
-import org.springframework.retry.policy.AlwaysRetryPolicy;
 import org.springframework.retry.policy.CircuitBreakerRetryPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.DefaultRetryState;
@@ -27,6 +23,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author lcc
+ */
 @SpringBootApplication
 public class SpringRetryApplication {
 
@@ -35,7 +34,6 @@ public class SpringRetryApplication {
   }
 
   @Bean
-  @Order(1)
   public ApplicationRunner runner(StateLessRetry stateLessRetry, StatefulRetry statefulRetry,
       CircuitBreakerRetry circuitBreakerRetry) {
     return args -> {
@@ -46,15 +44,14 @@ public class SpringRetryApplication {
         } catch (StatelessRetryException e) {
           System.out.println("捕获异常：" + e.getClass().getName());
         }
-        System.out.println(String.format(">>>>>第%s次无状态重试完毕", i));
+      }
+
+      for (int i = 0; i < 3; i++) {
+        System.out.println();
       }
 
       try {
-        for (int i = 0; i < 3; i++) {
-          System.out.println();
-        }
         System.out.println("================这是一条分割线==================");
-
         System.out.println("对 【StatefulRetryException】 异常进行有状态重试");
         for (; ; ) {
           try {
@@ -69,23 +66,30 @@ public class SpringRetryApplication {
       for (int j = 0; j < 3; j++) {
         System.out.println();
       }
-      System.out.println("================又是一条分割线==================");
 
+      System.out.println("================又是一条分割线==================");
       System.out.println("熔断重试策略");
       circuitBreakerRetry.test();
     };
+  }
+
+  private static class StatefulRetryException extends Exception {
+
+  }
+
+  private static class StatelessRetryException extends Exception {
+
   }
 
   @Component
   public static class StateLessRetry {
 
     private static final RetryTemplate TEMPLATE = new RetryTemplate();
-    int i = 1;
 
     static {
-      Map<Class<? extends Throwable>, Boolean> unRetryables = new HashMap<>();
-      unRetryables.put(StatelessRetryException.class, true);
-      SimpleRetryPolicy policy = new SimpleRetryPolicy(3, unRetryables);
+      Map<Class<? extends Throwable>, Boolean> retryables = new HashMap<>();
+      retryables.put(StatelessRetryException.class, true);
+      SimpleRetryPolicy policy = new SimpleRetryPolicy(3, retryables);
       TEMPLATE.setRetryPolicy(policy);
       TEMPLATE.setListeners(new RetryListener[]{
           new RetryListener() {
@@ -111,12 +115,17 @@ public class SpringRetryApplication {
       });
     }
 
+    int i = 1;
 
     void test() throws StatelessRetryException {
       TEMPLATE.execute(context -> {
         System.out.println(
-            String.format(">>>>无状态重试第%s次，执行线程：%s", i++, Thread.currentThread().hashCode()));
+            String.format(">>>>总共重试第%s次，执行线程：%s", i++, Thread.currentThread().hashCode()));
         throw new StatelessRetryException();
+      }, context -> {
+        System.out.println(">>执行回退策略");
+        System.out.println(context.getLastThrowable().getClass().getName());
+        return null;
       });
     }
   }
@@ -133,7 +142,7 @@ public class SpringRetryApplication {
       AtomicInteger atomicInteger = new AtomicInteger(1);
       TEMPLATE.execute(context -> {
         int i = atomicInteger.getAndIncrement();
-        System.out.println(String.format(">>>有状态重试第%s次，执行线程：%s", j++,
+        System.out.println(String.format(">>>总共重试第%s次，执行线程：%s", j++,
             Thread.currentThread().hashCode()));
         if (i % 2 == 0) {
           System.out.println("抛出异常：StatefulRetryException");
@@ -157,7 +166,7 @@ public class SpringRetryApplication {
     static {
       CircuitBreakerRetryPolicy policy = new CircuitBreakerRetryPolicy(new SimpleRetryPolicy());
       policy.setOpenTimeout(1000);
-      policy.setResetTimeout(5000L);
+      policy.setResetTimeout(20000);
       TEMPLATE.setRetryPolicy(policy);
     }
 
@@ -167,12 +176,15 @@ public class SpringRetryApplication {
         Thread.sleep(200L);
         try {
           TEMPLATE.execute(context -> {
-            System.out.println(String.format(">>>熔断重试第%s次，执行线程：%s", j++, Thread.currentThread().hashCode()));
-            System.out.println();
-            throw new IOException();
+            System.out.println(
+                String.format(">>>总共第%s次，执行线程：%s", j++, Thread.currentThread().hashCode()));
+            if (j % 2 == 0) {
+              throw new IOException();
+            }
+            return true;
           }, STATE);
         } catch (Exception e) {
-          System.out.println(e);
+          System.out.println("抛出异常：" + e.getClass());
         }
       }
     }
